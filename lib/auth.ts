77ea9a0e634cc1e27c0bf6f17f283fb0cd0bbbeb
yaredow <yaredyilma11@gmail.com>
2025-01-02 18:06:36 +0000

    fix google signin triggering credential login issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,4 +6,13 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
 });
