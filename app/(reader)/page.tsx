56334d61359035e07a37a0c1644f812dd50e8cc6
yaredow <yaredyilma11@gmail.com>
2025-01-25 19:31:11 +0300

    add a skeleton loader and move the data fetching to the client

--- app/(reader)/page.tsx
+++ app/(reader)/page.tsx
@@ -1,24 +1,5 @@
-import { BooksGrid } from "@/features/books/components/books-grid";
-import { getAllBooks } from "@/features/books/queries";
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
+import Home from "@/components/home";
 
 export default async function Page() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  const userId = session?.user?.id as string;
-
-  const books = await getAllBooks(userId);
-
-  if (!books || books.length === 0) {
-    return (
-      <div className=" items-center justify-center mx-auto">
-        No books available. Start by uploading
-      </div>
-    );
-  }
-
-  return <BooksGrid books={books} />;
+  return <Home />;
 }
---
+++ components/home.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useGetBooks } from "@/features/books/api/use-get-books";
+import { BooksGrid } from "@/features/books/components/books-grid";
+import BookCardSkeleton from "./skeletons/book-card-skeleton";
+
+export default function Home() {
+  const { books, isPending } = useGetBooks();
+
+  if (isPending) {
+    return <BookCardSkeleton />;
+  }
+
+  if (!books || books.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center text-center p-8 rounded-lg">
+        <div className="text-xl font-semibold text-gray-800 mb-4">
+          No books available. Start by uploading
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      <BooksGrid books={books} isPending={isPending} />
+    </main>
+  );
+}
