44df6b2bb1ef1133b053b67045c1e3ae67d5290a
yaredow <yaredyilma11@gmail.com>
2025-01-29 15:16:08 +0300

    display the closing button in maximized mode

--- components/responsive-reader-modal.tsx
+++ components/responsive-reader-modal.tsx
@@ -82,6 +82,28 @@ export default function ResponsiveReaderModal({
                   <Maximize2 className="h-4 w-4" />
                 )}
               </Button>
+              <Button
+                onClick={() => onOpenChange(false)}
+                variant="link"
+                size="icon"
+                className={`${theme === "dark" && "text-white"}`}
+              >
+                <span className="sr-only">Close</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </Button>
             </div>
           ) : (
             <div className="absolute top-2 right-2 z-10 flex items-center gap-2">
